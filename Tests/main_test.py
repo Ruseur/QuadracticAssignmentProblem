8e7e36a3387616f65fda15153f14ab333864da97
MattPoly <matthieu.cayet@etu.univ-lyon1.fr>
2019-05-05 15:58:33 +0200

    calcul voisinage

--- Tests/main_test.py
+++ Tests/main_test.py
@@ -1,5 +1,6 @@
 import unittest
 from Models.TaillardParser import TaillardParser
+from Services.Voisinage import Voisinage
 
 # https://docs.python.org/3/library/unittest.html
 class TestAllClasses(unittest.TestCase):
@@ -17,9 +18,11 @@ class TestAllClasses(unittest.TestCase):
         self.assertEqual(False, 43)
     
     def test_voisinage(self):
+        distances = self.parser.get_distance_matrix()
+        voisinage = Voisinage(distances)
         # in : X0   = 13452
         # out: V    = {43152, 12453, 13542, 13425}
-        self.assertEqual(False, {43152, 12453, 13542, 13425})
+        self.assertEqual(voisinage.get_voisins([0,2,3,4,1]), [[3,2,0,4,1], [0,2,4,3,1], [0,1,3,4,2], [0,2,3,1,4]])
 
     def test_recuit(self):
         # in : t0   = -12/ln(0.5)
